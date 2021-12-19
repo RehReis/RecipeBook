63da8d995e556aa0242c540c22b400c0303c82ba
Regina Grogan <regina.r.grogan@gmail.com>
2021-12-19 15:49:33 -0600

    Added Webpack and Babel to app.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Recipes!
+      </div>
+    )
+  }
+}
+
+export default App;
